85b45d57b0870b09e70d93b84265027630f70e14
avhhust <avgystunpolek@gmail.com>
2022-06-25 21:35:12 +0300

    version release 1.0

---
+++ scripts/navigate.js
@@ -0,0 +1,23 @@
+let arrButtons = document.querySelectorAll('.nav li');
+let arrSections = document.querySelectorAll('.slide');
+
+window.addEventListener("scroll", 
+function () {
+    let current = "";
+
+    arrSections.forEach((section) => {
+        let sectionTop = section.offsetTop;
+        if(this.window.pageYOffset >= sectionTop - 50){
+            current = section.getAttribute("id");
+        }
+    });
+
+    arrButtons.forEach((button) => {
+        button.classList.remove("active");
+        if(button.classList.contains(current)){
+            button.classList.add('active');
+        }
+    });
+
+}, false);
+
